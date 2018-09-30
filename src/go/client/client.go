abc92802a2765f305f34c99635bf7ccec9aa7c26
zergweak <zhang_anwen@163.com>
2018-09-30 16:08:52 +0800

    grpc connect ok

--- src/go/client/client.go
+++ src/go/client/client.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"google.golang.org/grpc"
+	"golang.org/x/net/context"
+	"log"
+	pb "../../gen"
+)
 
-func main()  {
-	fmt.Println("wallet clinet start!")
+const (
+	address     = "localhost:50052"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewWalletApiClient(conn)
+	var hash = []byte{0x12, 0x34, 0x56, 0x78, 0x9A}
+	r, err := c.Signature0(context.Background(), &pb.SignAddPassHashMessage{Address:"address", Password:"123456", Hash:hash})
+	if err != nil {
+		log.Fatal("Did not connect: %v", err)
+	}
+	log.Printf("Signature: %s", r.Signature)
 }
